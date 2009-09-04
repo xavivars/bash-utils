f46e84f8e87e0f9ce227d20abee85b7ecfdc7fba
Xavi Ivars <xavi.ivars@gmail.com>
2009-09-04 09:00:40 +0000

    git-svn-id: svn+ssh://infobenissa.com/home/xavivars/svn/bashutils@3 42c74a52-a96e-0410-9b88-8acb3b3618ef

--- get_correct_words.sh
+++ get_correct_words.sh
@@ -3,22 +3,42 @@
 LNG=$1
 TMP="/tmp/auxGCWtxt.tmp"
 LST="/tmp/auxGCWlist.tmp"
+CTMP="/tmp/auxcGCWtxt.tmp"
 
 IN=''
 Y=''
 SEP=''
+NUM=0
+
+echo -e '' > $LST
+echo -e '' > $CTMP
 
 while read Y;
 do
     IN="$IN$SEP$Y"
     SEP="\n"
+    let NUM=$NUM+1
+    if [ $NUM -eq 1000 ]; then
+
+        echo -e $IN > $TMP
+        echo -e $IN >> $CTMP
+
+        aspell --encoding=utf-8 -l $LNG list < $TMP >> $LST
+
+        IN=''
+        SEP=''
+
+        echo -e $NUM" words" >&2
+        NUM=0
+    fi
 done
 
 echo -e $IN > $TMP
+echo -e $IN >> $CTMP
 
-aspell --encoding=utf-8 -l $LNG list < $TMP > $LST
+aspell --encoding=utf-8 -l $LNG list < $TMP >> $LST
 
-deleteWords.py $LST < $TMP | sed -e '/^$/d'
+deleteWords.py $LST < $CTMP | sed -e '/^$/d'
 
 rm $TMP $LST
 
--- get_incorrect_words.sh
+++ get_incorrect_words.sh
@@ -6,11 +6,25 @@ TMP="/tmp/auxGCWtxt.tmp"
 IN=''
 Y=''
 SEP=''
+NUM=0
 
 while read Y;
 do
     IN="$IN$SEP$Y"
     SEP="\n"
+    let NUM=$NUM+1
+    if [ $NUM -eq 1000 ]; then
+
+        echo -e $IN > $TMP
+
+        aspell --encoding=utf-8 -l $LNG list < $TMP
+
+        IN=''
+        SEP=''
+
+        echo -e $NUM" words" >&2
+        NUM=0
+    fi
 done
 
 echo -e $IN > $TMP
